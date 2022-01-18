6b1dd33b4f2ac52e9c0e9db2b88110ad7a47ec05
home <home@WINDOWS-PQ21EPG>
2022-01-18 20:00:01 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Please enter a number:");
+        int a = Convert.ToInt32(Console.ReadLine());
+        if (a % 2 == 0)
+        {
+            Console.WriteLine("Value of a is even");
+        }
+        else
+        {
+            Console.WriteLine("Value of a is odd");
+        }
+    }
+}
